355ec0b050a5eeb1c42e7dab7cf60aa0cf3d3fd5
Blake <blakeandrewwood@gmail.com>
2015-09-10 06:53:10 +0900

    replaced div with section for component container

--- component/templates/_entry.js
+++ component/templates/_entry.js
@@ -1,11 +1,11 @@
 // <%= component_name %> Component
 document.write('<h3><%= component_name %></h3>');
-document.write('<div style="background-color: #fff; padding: 20px;">');
+document.write('<section class="framework-component">');
 // Component files 
 require("./components/<%= component_name %>/<%= component_name %>.js");
 require("!<%= css_prefix %>!<%= css_name %>!./components/<%= component_name %>/<%= component_name %>.<%= css_ext %>");
 var input = require("html!./components/<%= component_name %>/<%= component_name %>.html");
 document.write(input);
 // Component files End 
-document.write('</div>');
+document.write('</section>');
 //
